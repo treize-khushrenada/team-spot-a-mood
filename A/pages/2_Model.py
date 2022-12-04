94a1ad81d1ebb5e92118cb67129fbbec659d7004
Arthur C <78846238+treize-khushrenada@users.noreply.github.com>
2022-12-04 12:10:23 +0800

    Delete A/pages directory

--- A/pages/2_Model.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.markdown("# Our Information Retrieval Architect")
-st.sidebar.markdown("# Architect")
-#TODO: Methodology: your report explains how you attempted to solve the problem and justifies your methodological approach.
-#TODO: Technical depth: your report demonstrates mastery of learning objectives from multiple MADS courses.
-#TODO: Context: your report cites at least 3 studies, blogs, academic articles, or other sources that are relevant to your project. All references in your report are correctly formatted with a consistent citation style (such as MLA or APA). 
